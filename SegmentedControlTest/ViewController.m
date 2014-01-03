c5dc88e7dd0034ed491612cc49fb46dbde2d4620
steve0503 <leehr76@gmail.com>
2014-01-03 13:55:27 +0900

    한글로 쓰자.
    처음으로 올려본다.

--- SegmentedControlTest/ViewController.m
+++ SegmentedControlTest/ViewController.m
@@ -9,15 +9,87 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+
+    UISegmentedControl *segment;
+}
+
+- (IBAction)chooseNumber:(id)sender;
+
+
+
 
 @end
 
 @implementation ViewController
 
+- (IBAction)handleSegmentedSelection:(id)sender{
+    
+    UISegmentedControl *control = (UISegmentedControl *)sender;
+    
+    NSString *selectedStr = [control titleForSegmentAtIndex:control.selectedSegmentIndex];
+    
+    NSLog(@"Index:%ld - Title:%@",control.selectedSegmentIndex,selectedStr);
+    
+    
+}
+
+-(void)handleBallSelection:(UISegmentedControl *)sender{
+    
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            NSLog(@"야구!");
+            break;
+        case 1:
+            NSLog(@"농구!");
+            break;
+        case 2:
+            NSLog(@"축구!");
+            break;
+            
+        case 3 :
+            NSLog(@"배구!");
+            break;
+            
+        default:
+            break;
+    }
+    
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIImage *baseball = [UIImage imageNamed:@"button_left.jpeg"];
+    UIImage *basketball = [UIImage imageNamed:@"basketball.png"];
+    UIImage *football = [UIImage imageNamed:@"football.jpeg"];
+    UIImage *volleyball = [UIImage imageNamed:@"volleyball.jpeg"];
+    
+    NSArray *items = [NSArray arrayWithObjects:baseball,basketball,football,volleyball, nil];
+    
+ 
+    
+    segment = [[UISegmentedControl alloc] initWithItems:items];
+    
+    segment.frame = CGRectMake(10, 100, 300, 40);
+    
+    [setImage:forSegmentAtIndex:];
+    
+    [segment setImage: forSegmentAtIndex:1];
+    
+    [self.view addSubview:segment];
+    
+    segment.selectedSegmentIndex = 2;
+    
+    [segment addTarget:self action:@selector(handleBallSelection:) forControlEvents:UIControlEventValueChanged];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +98,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)chooseNumber:(id)sender {
+}
 @end
